9db258f9f5553df5a3ccb51afd6dff3688df9642
RaymonddC <raymondchrisandy@gmail.com>
2023-06-05 14:23:38 +0700

    fe login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import { Store } from './App/Store';
+
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={Store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function

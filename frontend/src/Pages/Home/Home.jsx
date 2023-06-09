babf9ce3dc7c41a308ca224757ac5e4e6b52f437
Andika Dwi Abizar <andikabizar2004@gmail.com>
2023-06-09 10:50:23 +0700

    CAM-12

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,33 @@
-import { FilterProduct } from '../../Components/FilterProduct/FilterProduct';
-import { Sidebar } from '../../Components/Sidebar/Sidebar';
+import { FilterProduct } from "../../Components/FilterProduct/FilterProduct";
+import { Sidebar } from "../../Components/Sidebar/Sidebar";
+import Card from "../../Components/Card/Card";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { getDataProduct } from "../../Features/product/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const { dataProduct, pageCount, isLoad } = useSelector(
+    (state) => state.product
+  );
+  const [page, setPage] = useState(1);
+  const [pageParams, setPageParams] = useSearchParams();
+  console.log(dataProduct);
+  useEffect(() => {
+    if (pageParams.get("page") === null) {
+      setPageParams(`page=1`);
+    }
+    dispatch(getDataProduct(pageParams.get("page")));
+    // setPage(Number(pageParams.get("page")));
+  }, []);
   return (
     <div className="flex bg-[#f0f0f0]">
       <Sidebar />
-      <FilterProduct />
-      <div className="w-full">home page</div>
+      <div className="w-full">
+        <FilterProduct />
+        <Card data={dataProduct} currentPage={pageParams.get("page")} />
+      </div>
     </div>
   );
 }

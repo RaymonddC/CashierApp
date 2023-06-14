eec2f4c7f92af0933b9871816f6a1877677591dc
RaymonddC <raymondchrisandy@gmail.com>
2023-06-14 10:14:46 +0700

    CAM-11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import { Toaster } from 'react-hot-toast';
+import { Route, Routes } from 'react-router-dom';
 
-import { Login } from "./Pages/Login/Login";
-import Home from "./Pages/Home/Home";
-import Admin from "./Pages/Admin/Admin";
-import { Category } from "./Pages/Admin/Category/Category";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { keepLoginAsync } from "./Features/User/UserSlice";
-import { CashierManagement } from "./Pages/Admin/CashierManagement/CashierManagement";
-import { Sidebar } from "./Components/Sidebar/Sidebar";
-import Dashboard from "./Pages/Dashboard/Dashboard";
+import { Login } from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
+import Admin from './Pages/Admin/Admin';
+import { Category } from './Pages/Admin/Category/Category';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { keepLoginAsync } from './Features/User/UserSlice';
+import { CashierManagement } from './Pages/Admin/CashierManagement/CashierManagement';
+import { Sidebar } from './Components/Sidebar/Sidebar';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import { ChangePassword } from './Pages/ChangePassword/ChangePassword';
 
 function App() {
   const dispatch = useDispatch();
@@ -26,16 +27,13 @@ function App() {
       <div className="content">
         <Sidebar />
         <Routes>
-          <Route path="/" element={<Home />} action={console.log("msuk")} />
-          <Route
-            path="/admin"
-            element={<Admin />}
-            action={console.log("admin")}
-          />
+          <Route path="/" element={<Home />} action={console.log('msuk')} />
+          <Route path="/admin" element={<Admin />} action={console.log('admin')} />
           <Route path="/login" element={<Login />} />
           <Route path="/categories" element={<Category />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/cashiers" element={<CashierManagement />} />
+          <Route path="/changePassword" element={<ChangePassword />} />
         </Routes>
       </div>
     </div>
--- frontend/src/Pages/ChangePassword/ChangePassword.jsx
+++ frontend/src/Pages/ChangePassword/ChangePassword.jsx
@@ -1,6 +1,10 @@
 import { Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import * as Yup from 'yup';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { onChangePass } from '../../Features/User/UserSlice';
 
 export const ChangePassword = () => {
   const DisplayingErrorMessagesSchema = Yup.object().shape({
@@ -10,72 +14,79 @@ export const ChangePassword = () => {
     //   .test('is-valid-type', 'Not a valid image type', (value) => isValidFileType(value && value.image.toLowerCase(), 'image')),
   });
 
+  const [showPass, setShowPass] = useState(false);
+  const [showCPass, setShowCPass] = useState(false);
+  const dispatch = useDispatch();
+
   return (
-    // <div>
-    //   <div className="formSide flex-1 flex flex-col h-[100vh] justify-center ">
-    //     <div className="form px-[20%] ">
-    //       <p className="text-[50px] ">Change Your Password</p>
-    //       <p className="text-[15px] pb-[20px] text-[#808080]">Change your password regularly</p>
-    //       <Formik
-    //         initialValues={{ password: '', confirmPassword: '' }}
-    //         validationSchema={DisplayingErrorMessagesSchema}
-    //         onSubmit={(values, { setSubmitting }) => {
-    //           setTimeout(() => {
-    //             dispatch(onLoginAsync(values));
-    //             // alert(JSON.stringify(values, null, 2));
-    //             setSubmitting(false);
-    //           }, 400);
-    //         }}
-    //       >
-    //         {({
-    //           values,
-    //           errors,
-    //           touched,
-    //           handleChange,
-    //           handleBlur,
-    //           handleSubmit,
-    //           isSubmitting,
-    //           /* and other goodies */
-    //         }) => (
-    //           <form onSubmit={handleSubmit} className="">
-    //             {/* <p className="font-semibold">E-mail</p> */}
-    //             <div className="inputEmail my-[20px]">
-    //               <input type="text" name="email" onChange={handleChange} placeholder="Enter Your e-mail" className="pl-[20px] pr-[15px] py-[20px] bg-[#EEEEEE] rounded-xl w-full " onBlur={handleBlur} value={values.email} />
-    //               {errors.email && touched.email && errors.email}
-    //             </div>
-    //             {/* <p className="font-semibold">Password</p> */}
-    //             <div className="inputPass relative my-[20px]">
-    //               <div className="icon" onClick={() => setShowPass(!showPass)}>
-    //                 {showPass ? <VisibilityOffOutlinedIcon className="absolute right-[20px] top-[20px]" /> : <VisibilityOutlinedIcon className="absolute right-[20px] top-[20px]" />}
-    //               </div>
-    //               <input
-    //                 type={showPass ? 'text' : 'password'}
-    //                 name="password"
-    //                 placeholder="Your Password"
-    //                 className="pl-[20px] pr-[15px] py-[20px] w-[100%] bg-[#EEEEEE] rounded-xl"
-    //                 onChange={handleChange}
-    //                 onBlur={handleBlur}
-    //                 value={values.password}
-    //               />
-    //               {errors.password && touched.password && errors.password}
-    //             </div>
-    //             {/* <Button variant="contained">Contained</Button> */}
-    //             <button type="submit" className="bg-[#FF2351] text-[white] rounded-xl py-[20px] w-full my-[20px]" disabled={isSubmitting}>
-    //               Submit
-    //             </button>
-    //           </form>
-    //         )}
-    //       </Formik>
-
-    //       <Link to={'/'}>
-    //         <p className="underline">Forgot Password?</p>
-    //       </Link>
-
-    //       <Link to={'/'}>
-    //         <p className="text-[#808080]">Don't have an account? Contact your Administrator</p>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
+    <div>
+      <div className="formSide flex-1 flex flex-col h-[100vh] justify-center ">
+        <div className="form px-[20%] ">
+          <p className="text-[50px] ">Change Your Password</p>
+          <p className="text-[15px] pb-[20px] text-[#808080]">Change your password regularly</p>
+          <Formik
+            initialValues={{ password: '', confirmPassword: '' }}
+            validationSchema={DisplayingErrorMessagesSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                dispatch(onChangePass(values));
+                // alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit} className="">
+                {/* <p className="font-semibold">E-mail</p> */}
+                <div className="inputPass relative my-[20px]">
+                  <div className="icon" onClick={() => setShowPass(!showPass)}>
+                    {showPass ? <VisibilityOffOutlinedIcon className="absolute right-[20px] top-[20px]" /> : <VisibilityOutlinedIcon className="absolute right-[20px] top-[20px]" />}
+                  </div>
+                  <input
+                    type={showPass ? 'text' : 'password'}
+                    name="password"
+                    placeholder="Your Password"
+                    className="pl-[20px] pr-[15px] py-[20px] w-[100%] bg-[#EEEEEE] rounded-xl"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  {errors.password && touched.password && errors.password}
+                </div>
+                {/* <p className="font-semibold">Password</p> */}
+                <div className="inputPass relative my-[20px]">
+                  <div className="icon" onClick={() => setShowCPass(!showCPass)}>
+                    {showCPass ? <VisibilityOffOutlinedIcon className="absolute right-[20px] top-[20px]" /> : <VisibilityOutlinedIcon className="absolute right-[20px] top-[20px]" />}
+                  </div>
+                  <input
+                    type={showCPass ? 'text' : 'password'}
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    className="pl-[20px] pr-[15px] py-[20px] w-[100%] bg-[#EEEEEE] rounded-xl"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.confirmPassword}
+                  />
+                  {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}
+                </div>
+                {/* <Button variant="contained">Contained</Button> */}
+                <button type="submit" className="bg-[#FF2351] text-[white] rounded-xl py-[20px] w-full my-[20px]" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
   );
 };
